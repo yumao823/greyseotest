ccc48709ac08db86a93f7a74a6cb00f00453c604
Koichi <koichi.p.noda@gmail.com>
2022-02-25 13:16:27 +0900

    feat: add sign up form

--- views/Test2.tsx
+++ views/Test2.tsx
@@ -1,10 +1,60 @@
-import React, { FC } from 'react'
-import { Container } from 'react-bootstrap'
+import React, { FC, useState } from 'react'
+import { Container, Form, Row, Col, InputGroup } from 'react-bootstrap'
 
 const Test2: FC = () => {
+  const [name, setName] = useState<string>('')
+  const [country, setCountry] = useState<string>('')
+  const [school, setSchool] = useState<string>('')
+  const [area, setArea] = useState<string>('')
+
+
   return (
-    <Container>
-      Test2
+    <Container className='test2'>
+      <div className='signup-form mx-auto mt-5 p-4'>
+        <Form.Group className="mb-3">
+          <Form.Label className='title'>Your full given name:</Form.Label>
+          <Form.Control value={name} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}/>
+        </Form.Group>
+        <Row>
+          <Col sm={5}>
+            <Form.Group className="mb-3">
+              <Form.Label className='title'>Date of Birth</Form.Label>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>
+                  <img src='/date.svg' />
+                </InputGroup.Text>
+                <Form.Control />
+              </InputGroup>
+            </Form.Group>
+          </Col>
+          <Col sm={7}>
+          <Form.Group className="mb-3">
+            <Form.Label className='title'>
+              Country of resident or citizenship:
+            </Form.Label>
+            <Form.Control value={country} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCountry(e.target.value)} />
+          </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group className="mb-3">
+          <Form.Label className='title'>
+            What school do you plan to attend:
+          </Form.Label>
+          <Form.Control value={school} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSchool(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label className='title'>
+            Please take a moment to describe your intended area of study:
+          </Form.Label>
+          <Form.Control
+            value={area}
+            as="textarea"
+            placeholder="Enter Details here"
+            rows={5}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setArea(e.target.value)}
+          />
+        </Form.Group>
+      </div>
     </Container>
   )
 }
--- views/Test3.tsx
+++ views/Test3.tsx
@@ -1,11 +1,45 @@
 import React, { FC } from 'react'
-import { Container } from 'react-bootstrap'
+
+
 
 const Test3: FC = () => {
+  
+  // if (recipe == 'SPANISH') {
+  //   fudge = SPANISH_FUDGE;
+  //   amt = base * SPANISH_FUDGE;
+  //   sugar = 2 * bottom(amt) + top(amt) * 1.17;
+  // } else if ((recipe == 'FRENCH') || (recipe == 'ENGLISH')) {
+  //     fudge = (recipe == 'FRENCH') ? FRENCH_FUDGE : ENGLISH_FUDGE;
+
+  //     amt = base * fudge;
+  //     sugar = 2 * bottom(amt) + top(amt) * 1.17;
+  //     if (recipe == 'FRENCH') {
+  //         chocolate = 7;
+  //     }
+  // } else {
+  //     fudge = 1;
+  //     amt = base;
+  //     sugar = 2 * bottom(amt) + top(amt) * 1.17;
+  // }
+
+  /**
+   * Below is Correct one
+   */
+
+
+  // switch (recipe) {
+  //   case 'SPANISH': fudge = SPANISH_FUDGE; break;
+  //   case 'FRENCH': fudge = FRENCH_FUDGE; chocolate = 7; break;
+  //   case 'ENGLISH': fudge = ENGLISH_FUDGE; break;
+  //   default fudge = 1; break;
+  // }
+
+  // amt = base * fudge;
+  // sugar = 2 * bottom(amt) + top(amt) * 1.17;
+ 
+
   return (
-    <Container>
-      Test3
-    </Container>
+    <h1>Please check the Answer from views/Test3.tsx</h1>
   )
 }
 
